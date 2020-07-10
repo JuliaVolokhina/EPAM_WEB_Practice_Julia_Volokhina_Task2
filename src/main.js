225ed8d81a65e7a3dc830f13c1b3c48fb1192cd0
JuliaVolokhina <1999julia@mail.ru>
2020-07-10 22:46:28 +0400

    функции для кнопок 2, 3

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-//var io = require("socket.io-client")
-//var socket = io.connect("https://voicy-speaker.herokuapp.com/");
-
-//var users = io.sockets.clients();
-//console.log(users);
-
-//socket.on('audioMessage', function (audioChunks) {
-//  const audioBlob = new Blob(audioChunks);
-//  const audioUrl = URL.createObjectURL(audioBlob);
-//  const audio = new Audio(audioUrl);
-//  audio.play();
-//});
+var io = require("socket.io-client")
+//import io from 'socket.io-client';
+var socket = io.connect("https://voicy-speaker.herokuapp.com/");
+//const socket = io('https://voicy-speaker.herokuapp.com/');
 
 function button1() {
   response = document.getElementById("response"),
@@ -22,13 +14,42 @@ function button2() {
   response = document.getElementById("response"),
     output = "Active: speaker-mode";
   response.innerHTML = output;
-  //socket.emit('audioMessage', audioChunks);
 }
 document.getElementById("microphoneid").addEventListener("click", button2);
+document.getElementById("microphoneid").addEventListener("click", button2_recordandsend);
+
+function button2_recordandsend() {
+  navigator.mediaDevices.getUserMedia({ audio: true})
+    .then(stream => {
+        const mediaRecorder = new MediaRecorder(stream);
+        var audioChunks = [];
+        audioChunks.push(event.data);
+        socket.emit("audioMessage", audioChunks);
+        mediaRecorder.start();
+
+        document.getElementById("microphoneid").addEventListener("click", stopRecord);
+    });
+}
+
+function stopRecord() {
+  mediaRecorder.stopRecord
+}
 
 function button3() {
   response = document.getElementById("response"),
     output = "Active: listener-mode";
   response.innerHTML = output;
+  button3_stream()
 }
 document.getElementById("streamid").addEventListener("click", button3);
+
+function button3_stream() {
+  socket.on('audioMessage', function (audioChunks) {
+  const audioBlob = new Blob(audioChunks);
+  const audioUrl = URL.createObjectURL(audioBlob);
+  const audio = new Audio(audioUrl);    
+  audio.play();
+  console.log("Message streamed!");
+});
+}
+//document.getElementById("microphoneid").addEventListener("click", button3_stream);
--- src/user.js
+++ src/user.js
@@ -1,18 +1,9 @@
 var io = require("socket.io-client")
+//import io from 'socket.io-client';
 var socket = io.connect("https://voicy-speaker.herokuapp.com/");
-
-var users = io.sockets.clients();
-console.log(users);
+//const socket = io('https://voicy-speaker.herokuapp.com/');
 
 socket.on('connect', function() {
     console.log("Connection to the server is successfull!");
 });
 
-socket.on('audioMessage', function (audioChunks) {
-    const audioBlob = new Blob(audioChunks);
-    const audioUrl = URL.createObjectURL(audioBlob);
-    const audio = new Audio(audioUrl);
-    audio.play();
-});
-
-socket.emit('audioMessage', audioChunks);
