9fe15fdf2132af9aabfe94ec2d184df9e9e1566f
JuliaVolokhina <1999julia@mail.ru>
2020-07-14 22:01:54 +0400

    server part

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./main.js";
-import "./style.scss";
-import "./user.js";
-const user = require("./user.js");
--- src/user.js
+++
@@ -1,9 +0,0 @@
-var io = require("socket.io-client")
-//import io from 'socket.io-client';
-var socket = io.connect("https://voicy-speaker.herokuapp.com/");
-//const socket = io('https://voicy-speaker.herokuapp.com/');
-
-socket.on('connect', function() {
-    console.log("Connection to the server is successfull!");
-});
-
